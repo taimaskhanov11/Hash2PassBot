72da77a68f98a898d8e35cbe1f7d1554969956ab
Rasul Taimaskhanov <81189712+taimaskhanov11@users.noreply.github.com>
2022-06-03 20:04:09 +0300

    minor fix

--- run_bot.bash
+++ run_bot.bash
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 poetry run uvicorn api_server:app --port 8001 --app-dir=hash2passbot/api &
-poetry run python user_database_tg/main.py && fg
+poetry run python hash2passbot/main.py && fg
