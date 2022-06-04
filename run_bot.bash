2687b290873fef9a4b748af78dce30edd0eb085f
Rasul Taimaskhanov <81189712+taimaskhanov11@users.noreply.github.com>
2022-06-04 15:31:58 +0300

    minor fix

--- run_bot.bash
+++ run_bot.bash
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # killall gcalctool /home/user/.cache/pypoetry/virtualenvs/hash2passbot-vOgQLhOe-py3.10/bin/python &&
-pkill -f hash2passbot/main.py &
+# pkill -f hash2passbot/main.py &
 poetry run python hash2passbot/main.py &
 poetry run uvicorn api_server:app --port 8001 --app-dir=hash2passbot/apps/api && fg
